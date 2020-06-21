28a7b2cc4a23aa43b9d095bdad93b553bccb1a1f
liuxu <i@liuxu.me>
2020-06-21 17:14:06 +0800

    个人使用，删除多余的代码

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-
-cat >  ./v2ray_deploy/manifest.yml  << EOF
-applications:
-  - path: .
-    name: liuxu-v2ray 
-    random-route: true
-    memory: 256M
-EOF
-
 git clone https://github.com/liuquanhao/ibm_v2ray
 
 cd ./ibm_v2ray/v2ray_deploy
