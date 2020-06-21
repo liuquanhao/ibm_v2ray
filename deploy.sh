0ea7434b2872742d1482c03aa25ee7de57645101
liuxu <i@liuxu.me>
2020-06-21 16:51:23 +0800

    uuid改为github secrets设置

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,6 @@ EOF
 git clone https://github.com/liuquanhao/ibm_v2ray
 
 cd ./ibm_v2ray/v2ray_deploy
-UUID=$(cat /proc/sys/kernel/random/uuid)
-sed -i "s/id\": .*\"/id\": \"$UUID\"/g" ./v2ray/config.json
 ibmcloud target --cf
 ibmcloud cf install
 ibmcloud cf push
