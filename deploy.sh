9a13d45b11e8c62731d43dff219b1284045fea26
liuxu <i@liuxu.me>
2020-06-21 19:14:57 +0800

    调整uuid为部署时生成

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/liuquanhao/ibm_v2ray
+UUID=$(cat /proc/sys/kernel/random/uuid)
+echo "uuid: " $UUID
 
+git clone https://github.com/liuquanhao/ibm_v2ray
 cd ./ibm_v2ray/v2ray_deploy
-chmod +x v2ray/v2ray v2ray/v2ctl
+sed -i "s/id\": .*\"/id\": \"$UUID\"/g" ./v2ray/config.json
 ibmcloud target --cf
 ibmcloud cf install
 ibmcloud cf push
